c747e0c4ecb752430cece43dd338ffbec9fb8d03
Hima29nshi <s1032190300@gmail.com>
2022-07-27 14:05:34 +0530

    first commit

---
+++ app/src/main/java/com/example/newsapp/DetailsActivity.java
@@ -0,0 +1,62 @@
+package com.example.newsapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.newsapp.Models.Headlines;
+import com.google.android.material.navigation.NavigationView;
+import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
+
+public class DetailsActivity extends AppCompatActivity {
+    Headlines headlines;
+    TextView txt_title,txt_author,txt_time,txt_detail,txt_content;
+    ImageView img_news;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        txt_title=findViewById(R.id.text_detail_title);
+        txt_author=findViewById(R.id.text_detail_author);
+        txt_detail=findViewById(R.id.text_detail_detail);
+        txt_content=findViewById(R.id.text_detail_content);
+        txt_time=findViewById(R.id.text_detail_time);
+        img_news=findViewById(R.id.img_detail_news);
+        toolbar = findViewById(R.id.tool);
+
+        headlines = (Headlines) getIntent().getSerializableExtra("data");
+
+        txt_title.setText(headlines.getTitle());
+        txt_author.setText(headlines.getAuthor());
+        txt_time.setText(headlines.getPublishedAt());
+        txt_detail.setText(headlines.getDescription());
+        txt_content.setText(headlines.getContent());
+        Picasso.get().load(headlines.getUrlToImage()).into(img_news);
+
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_ios_24);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+
+}
